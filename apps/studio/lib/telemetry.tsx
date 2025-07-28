f8cd9e261fc0b0949c4dd552a75694544a65df07
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2025-07-28 11:52:02 -0700

    chore: align client-side ID with server side ID for event deduplication

--- apps/studio/lib/telemetry.tsx
+++ apps/studio/lib/telemetry.tsx
@@ -1,4 +1,4 @@
-import { PageTelemetry, posthogClient } from 'common'
+import { PageTelemetry, posthogClient, useUser, useParams } from 'common'
 import GroupsTelemetry from 'components/ui/GroupsTelemetry'
 import { API_URL, IS_PLATFORM } from 'lib/constants'
 import { useConsentToast } from 'ui-patterns/consent'
@@ -10,12 +10,23 @@ export function Telemetry() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { hasAcceptedConsent } = IS_PLATFORM ? useConsentToast() : { hasAcceptedConsent: true }
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const user = IS_PLATFORM ? useUser() : null
+
   // Initialize PostHog client when telemetry is enabled
   useEffect(() => {
     if (IS_PLATFORM) {
       posthogClient.init()
     }
   }, [])
+
+  // Identify user in PostHog when they're logged in
+  useEffect(() => {
+    if (IS_PLATFORM && hasAcceptedConsent && user?.id && typeof window !== 'undefined') {
+      // Use same user ID as backend to unify events
+      posthogClient.identify(user.id)
+    }
+  }, [hasAcceptedConsent, user?.id])
 
   return (
     <>
@@ -24,7 +35,7 @@ export function Telemetry() {
         hasAcceptedConsent={hasAcceptedConsent}
         enabled={IS_PLATFORM}
       />
-      <GroupsTelemetry hasAcceptedConsent={hasAcceptedConsent} />
+      {/* <GroupsTelemetry hasAcceptedConsent={hasAcceptedConsent} /> */}
     </>
   )
 }
